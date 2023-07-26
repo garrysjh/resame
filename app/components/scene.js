1cf25e1650812fe0abcfca5af6154e9ed14ba461
garrysjh <garrysjh@gmail.com>
2023-07-26 16:03:35 +0800

    changed shape, added directional & ambient light,

--- app/components/scene.js
+++ app/components/scene.js
@@ -7,7 +7,7 @@ export default function MainScene() {
     React.useEffect(() => {
         var canvas = document.querySelector('displayContent');
         const scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xff0000);
+        scene.background = new THREE.Color(0xF5F5DC);
         const camera = new THREE.PerspectiveCamera(
             75,
             window.innerWidth / window.innerHeight,
@@ -19,10 +19,25 @@ export default function MainScene() {
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
+        const geometry = new THREE.TorusKnotGeometry(1);
+        const material = new THREE.MeshPhongMaterial({shininess: 150, color: 'purple'});
+        const sphere = new THREE.Mesh(geometry, material);
+        scene.add(sphere);
+        
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.AmbientLight(color, intensity);
+        scene.add(light);
+
+        // const pointLight = new THREE.PointLight( 0xffffff );
+        // pointLight.position.set(1,1,2);
+        // camera.add(pointLight);
+
+        const directionalLight = new THREE.DirectionalLight(color, 5);
+        directionalLight.position.set(0, 10, 0);
+        directionalLight.target = sphere
+        scene.add(directionalLight)
+
 
         camera.position.z = 5;
 
@@ -40,9 +55,10 @@ export default function MainScene() {
 
         function animate() {
             requestAnimationFrame(animate);
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+            sphere.rotation.x += 0.00125;
+            sphere.rotation.y += 0.00125;
             renderer.render(scene, camera);
+
         }
         animate();
     })
